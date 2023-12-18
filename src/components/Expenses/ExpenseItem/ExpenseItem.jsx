445f780256618e9a02c08382e446cdf59ac3910b
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 03:58:10 +0530

    Create ExpenseItem.jsx

---
+++ src/components/Expenses/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,28 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
+import ExpenseDetails from "../ExpenseDetails";
+import Card from "../../UI/Card";
+
+const ExpenseItem = (props) => {
+  const deleteHandler = (id) => {
+    const result = window.confirm("Are you Sure?");
+    result && props.delete(id);
+  };
+
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <ExpenseDetails title={props.title} amount={props.amount} />
+        <button
+          className="expense-item__delete"
+          onClick={() => deleteHandler(props.id)}
+        >
+          X
+        </button>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
